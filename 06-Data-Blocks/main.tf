32067b9b5c27d1a5a8815f073c2ab4bb28bd85ac
Ebuka Odini <ebukaodini@gmail.com>
2025-07-21 09:02:54 +0100

    chore: completed lesson six; update AWS provider configuration for LocalStack testing and add new AMI data sources

--- 06-Data-Blocks/main.tf
+++ 06-Data-Blocks/main.tf
@@ -13,6 +13,18 @@ terraform {
 
 provider "aws" {
   region = var.region
+
+  # For LocalStack testing
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+  endpoints {
+    iam = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -20,7 +32,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
 
   filter {
@@ -29,6 +41,23 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = ["099720109477"] # Canonical https://ubuntu.com/server/docs/cloud-images/amazon-ec2
+}
+
+data "aws_ami" "linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon Linux
+
 }
 
 resource "aws_instance" "webserver" {
